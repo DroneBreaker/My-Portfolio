c1461e2fd863984321f077cebc81b6c5c1ab9414
DroneBreaker <brownierz01@gmail.com>
2025-02-12 08:26:20 +0000

    Initialized my portfolio

---
+++ src/components/Hero.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="min-h-screen flex flex-col justify-center relative px-4 sm:px-6 lg:px-8">
+        <div class="text-center">
+            <span>
+                <h1 class="text-3xl">Richard Browne</h1>
+            </span>
+        </div>
+    </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
