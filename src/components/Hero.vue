4a3d7eb301c76076029a7281e3f779c5a012a7d5
DroneBreaker <brownierz01@gmail.com>
2025-02-18 14:45:15 +0000

    Test

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,15 +1,32 @@
 <template>
     <section class="min-h-screen flex flex-col justify-center relative px-4 sm:px-6 lg:px-8">
         <div class="text-center">
-            <span>
-                <h1 class="text-3xl">Richard Browne</h1>
-            </span>
+            <h1  class="text-5xl sm:text-6xl font-bold mb-6 tracking-tight">
+                <span class="bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-500">
+                  Richard Browne
+                </span>
+            </h1>
+
+            <p class="text-2xl sm:text-3xl text-gray-400 mb-8">Software Engineer</p>
+
+            <!-- Icons -->
+            <div class="flex justify-center gap-6 mb-12">
+            <a href="https://github.com/DroneBreaker" class="text-gray-400 hover:text-purple-400 transition-colors">
+              <Github class="w-6 h-6" />
+            </a>
+            <a href="https://linkedin.com/in/therichardbrowne" class="text-gray-400 hover:text-purple-400 transition-colors">
+              <Linkedin class="w-6 h-6" />
+            </a>
+            <a href="mailto:brownierz01@gmail.com" class="text-gray-400 hover:text-purple-400 transition-colors">
+              <Mail class="w-6 h-6" />
+            </a>
+          </div>
         </div>
     </section>
 </template>
 
 <script setup>
-
+import { Github, Linkedin, Mail, ExternalLink, Send } from 'lucide-vue-next'
 </script>
 
 <style lang="scss" scoped>
