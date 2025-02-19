8660c6c4c843f3e396b1afb9ab976ad179e8c4c2
DroneBreaker <brownierz01@gmail.com>
2025-02-19 10:33:24 +0000

    Added my resume to it to be downloaded

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,24 +1,29 @@
 <template>
     <section class="min-h-screen flex flex-col justify-center relative px-4 sm:px-6 lg:px-8">
         <div class="text-center">
-            <h1  class="text-5xl sm:text-7xl font-bold mb-4 tracking-tight">
-                <span class="bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-500">
-                  Richard Browne
-                </span>
-            </h1>
+          <h1  class="text-5xl sm:text-7xl font-bold mb-4 tracking-tight">
+              <span class="bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-500">
+                Richard Browne
+              </span>
+          </h1>
 
-            <p class="text-2xl sm:text-3xl text-gray-400 mb-4">Software Engineer</p>
+          <p class="text-2xl sm:text-3xl text-gray-400 mb-4">Software Engineer</p>
 
-            <!-- Icons -->
-            <div class="flex justify-center gap-6 mb-12">
+          <!-- Icons -->
+          <div class="flex justify-center gap-6 mb-12">
             <a href="https://github.com/DroneBreaker" class="text-gray-400 hover:text-purple-400 transition-colors">
               <Github class="w-6 h-6" />
             </a>
+            
             <a href="https://linkedin.com/in/therichardbrowne" class="text-gray-400 hover:text-purple-400 transition-colors">
               <Linkedin class="w-6 h-6" />
             </a>
+
             <a href="mailto:brownierz01@gmail.com" class="text-gray-400 hover:text-purple-400 transition-colors">
               <Mail class="w-6 h-6" />
+            </a>
+            <a href="/src/assets/richardbrowne.pdf" download="/src/assets/richardbrowne.pdf" class="text-gray-400 hover:text-purple-400 transition-colors">
+              <File class="w-6 h-6" />
             </a>
           </div>
         </div>
@@ -26,7 +31,7 @@
 </template>
 
 <script setup>
-import { Github, Linkedin, Mail, ExternalLink, Send } from 'lucide-vue-next'
+import { Github, Linkedin, Mail, ExternalLink, Send, File } from 'lucide-vue-next'
 </script>
 
 <style lang="scss" scoped>
